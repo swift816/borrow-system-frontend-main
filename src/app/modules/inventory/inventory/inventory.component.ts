05f0b170066d8c4ee9afe969713cb97005b93315
Neil Vincent Donaire <84239783+swift816@users.noreply.github.com>
2024-04-16 22:37:11 +0800

    changes

--- src/app/modules/inventory/inventory/inventory.component.ts
+++ src/app/modules/inventory/inventory/inventory.component.ts
@@ -1,19 +1,22 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationExtras, Params, Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-inventory',
   templateUrl: './inventory.component.html',
   styleUrls: ['./inventory.component.css']
 })
-export class InventoryComponent {
-  constructor(private authService: AuthService, private router: Router) { }
+export class InventoryComponent implements OnInit{
+  constructor(private authService: AuthService, private router: Router, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
     const currentUser = this.authService.getCurrentUser();
     if (!currentUser || !this.isAllowedRole(currentUser.role)) {
       this.router.navigate(['/']);
     }
+    this.activatedRoute. queryParams.subscribe((params: Params)=>{
+
+    })
   }
   isAdmin(): boolean {
     const currentUser = this.authService.getCurrentUser();
@@ -24,6 +27,15 @@ export class InventoryComponent {
     return currentUser ? currentUser.role === 'reads' : false;
   }
   
+  onFilterSelect(event: any){
+    let filter = event.filtername;
+    let value = event.value
+    const naviationExtras: NavigationExtras = {
+      queryParams: {
+        [filter]: value
+      },
+    };
+  }
   private isAllowedRole(role: string): boolean {
     const allowedRoles = ['Admin', 'Instructor', 'reads', 'oic', 'faculty'];
     return allowedRoles.includes(role);
